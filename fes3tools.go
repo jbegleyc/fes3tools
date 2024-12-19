6e6436b456764e706da3313742e526a5d80aedac
jbegleyc <jbegleyc@me.com>
2024-12-19 10:13:02 -0500

    add GetFile function

--- fes3tools.go
+++ fes3tools.go
@@ -57,3 +57,32 @@ func GetComInfo(bucket string, partner string) ComInfo {
 
 	return c
 }
+
+func GetFile(bucket string, fname string) string {
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
+
+	output, err := client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(fname),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer output.Body.Close()
+
+	// Read the file content into memory
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, output.Body)
+	if err != nil {
+		fmt.Println("Error reading object body:", err)
+	}
+
+	return buf.String()
+}
